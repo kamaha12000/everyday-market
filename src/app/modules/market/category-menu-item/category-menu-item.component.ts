3e7a045a2954e3e48b075003fd8a4779e473c2bb
kamaha12000 <kamaha12000@yahoo.fr>
2019-01-01 21:45:23 -0800

    Added category-menu-item component

---
+++ src/app/modules/market/category-menu-item/category-menu-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-menu-item',
+  templateUrl: './category-menu-item.component.html',
+  styleUrls: ['./category-menu-item.component.css']
+})
+export class CategoryMenuItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/market/market.module.ts
+++ src/app/modules/market/market.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CategoryMenuItemComponent } from './category-menu-item/category-menu-item.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CategoryMenuItemComponent],
   imports: [
     CommonModule
   ]
