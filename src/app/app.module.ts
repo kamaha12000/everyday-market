fd6537da2fd315d2b0cd7533c649f153f8f375a5
kamaha12000 <kamaha12000@yahoo.fr>
2019-01-01 21:41:19 -0800

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './modules/shared/header/header.component';
+import { SharedModule } from './modules/shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
